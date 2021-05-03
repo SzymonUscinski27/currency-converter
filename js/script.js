d05fbb073579650a517417749102175e5910d39b
Szymon Uscinski <szymonuscinski@gmail.com>
2021-05-03 17:43:25 +0200

    added initial and on form submit functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 {
-    const formElement = document.querySelector(".js-form");
-    const amountElement = document.querySelector(".js-amount");
-    const currencyElement = document.querySelector(".js-currency");
-    const resultElement = document.querySelector(".js-result");
-    const resetButton = document.querySelector(".js-resetButton");
-
     const calculateFinalCurrency = (amount, currency) => {
         const rateEUR = 4.5546;
         const rateUSD = 3.8014;
@@ -25,12 +19,18 @@
         }
     }
 
-    formElement.addEventListener("submit", (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
+
+        const resetButton = document.querySelector(".js-resetButton");
 
         resetButton.addEventListener("click", () => {
             location.reload();
         });
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
 
         let amount = +amountElement.value;
         let currency = currencyElement.value;
@@ -38,6 +38,13 @@
         let result = calculateFinalCurrency(amount, currency);
 
         resultElement.innerHTML = `<strong>${amount} PLN = ${result.toFixed(2)} ${currency}</strong>`;
-    });
+    };
 
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
 }
